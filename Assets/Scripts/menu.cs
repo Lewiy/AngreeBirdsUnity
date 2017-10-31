8a8c093e10ec2b097a8d282897a2b692a953de82
Lev <romanenko.lew@gmail.com>
2017-10-31 14:31:52 +0200

    Main part was complited
    The base part of project was created

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour {
+
+	public void startGame()
+    {
+        Debug.Log("Press logs start");
+        SceneManager.LoadScene(2);
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    public void continueF()
+    {
+
+    }
+}
