024616d8f540fbdb1cada0d92fc275ca1f966a76
Lev <romanenko.lew@gmail.com>
2017-12-06 16:45:12 +0200

    There was added download function of  picture and nickname from facebook account

--- Assets/FBscript.cs
+++ Assets/FBscript.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using Facebook.Unity;
 using System.Collections.Generic;
 
 public class FBscript : MonoBehaviour {
 
+    public GameObject DialogLoggedIn;
+    public GameObject DialogLoggedOut;
+    public GameObject DialogUserName;
+    public GameObject DialogProfilePic;
 	void Awake()
     {
         FB.Init(SetInit,OnHideUnity);////// bug
@@ -19,6 +24,8 @@ public class FBscript : MonoBehaviour {
         {
             Debug.Log("FB is not log in");
         }
+
+        DealWithFBMenus(FB.IsLoggedIn);
     }
 
     void OnHideUnity(bool isGameShown)
@@ -50,10 +57,55 @@ public class FBscript : MonoBehaviour {
             if (FB.IsLoggedIn)
             {
                 Debug.Log("FB is logged in");
+                
             }else
             {
                 Debug.Log("FB is not logged in");
             }
+            DealWithFBMenus(FB.IsLoggedIn);
         }
     }
+
+    void DealWithFBMenus(bool isLoggedIn)
+    {
+        if (isLoggedIn)
+        {
+            DialogLoggedIn.SetActive(true);
+            DialogLoggedOut.SetActive(false);
+
+            FB.API("/me?fields=first_name", HttpMethod.GET, DisplayUsername);
+            FB.API("/me/picture?type=square&height=128&width=128",HttpMethod.GET,DisplayProfilePic);
+        }else
+        {
+            DialogLoggedIn.SetActive(false);
+            DialogLoggedOut.SetActive(true);
+        }
+
+    }
+
+    void DisplayUsername(IResult result)
+    {
+        Text UserName = DialogUserName.GetComponent <Text>();
+
+        if (result.Error == null)
+        {
+            UserName.text = "Hi, there, " + result.ResultDictionary["first_name"];
+            Debug.Log("Hi, there, " + result.ResultDictionary["first_name"]);
+        }
+        else
+        {
+            Debug.Log(result.Error);
+        }
+    }
+
+    void DisplayProfilePic(IGraphResult results)
+    {
+        if(results.Texture != null)
+        {
+            Image ProfilePic = DialogProfilePic.GetComponent<Image>();
+
+            ProfilePic.sprite = Sprite.Create(results.Texture, new Rect(0,0,128,128),new Vector2());
+        }
+    }
+
 }
